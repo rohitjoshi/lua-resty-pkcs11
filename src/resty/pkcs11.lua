8ef2083cb7d958eded4c17d16995a5729a3c43fd
Marek Bazler <marek.bazler@capitalone.com>
2016-12-14 17:34:34 -0500

    Login added

--- src/resty/pkcs11.lua
+++ src/resty/pkcs11.lua
@@ -3,14 +3,20 @@ local ffi = require("ffi")
 ffi.cdef[[ 
   typedef void*             CK_VOID_PTR;
 
+  typedef unsigned char     CK_BYTE;
   typedef unsigned long int CK_ULONG;
   
+  typedef CK_BYTE           CK_UTF8CHAR;
+
   typedef CK_ULONG          CK_RV;
   typedef CK_ULONG          CK_SLOT_ID;
   typedef CK_ULONG          CK_FLAGS;
   typedef CK_ULONG          CK_SESSION_HANDLE;
   typedef CK_SESSION_HANDLE * CK_SESSION_HANDLE_PTR;
   typedef CK_ULONG          CK_NOTIFICATION;
+  typedef CK_ULONG          CK_USER_TYPE;  
+
+  typedef CK_UTF8CHAR       * CK_UTF8CHAR_PTR;
 
   typedef CK_RV (* CK_NOTIFY)(
       CK_SESSION_HANDLE hSession,   
@@ -33,32 +39,67 @@ ffi.cdef[[
       CK_SESSION_HANDLE hSession
   );
 
+  CK_RV C_Login (
+      CK_SESSION_HANDLE hSession,  /* the session's handle */
+      CK_USER_TYPE      userType,  /* the user type */
+      CK_UTF8CHAR_PTR   pPin,      /* the user's PIN */
+      CK_ULONG          ulPinLen   /* the length of the PIN */
+  );
+
+  CK_RV C_Logout (
+      CK_SESSION_HANDLE hSession
+  );
 ]]
 
 
-local CKR_OK                            = 0x00000000
-local CKR_HOST_MEMORY                   = 0x00000002
-local CKR_GENERAL_ERROR                 = 0x00000005
-local CKR_FUNCTION_FAILED               = 0x00000006
-local CKR_ARGUMENTS_BAD                 = 0x00000007
-local CKR_NEED_TO_CREATE_THREADS        = 0x00000009
-local CKR_CANT_LOCK                     = 0x0000000A
-local CKR_CRYPTOKI_ALREADY_INITIALIZED  = 0x00000191
+local CKR_OK                             = 0x00000000
+local CKR_HOST_MEMORY                    = 0x00000002
+local CKR_SLOT_ID_INVALID                = 0x00000003
+local CKR_GENERAL_ERROR                  = 0x00000005
+local CKR_FUNCTION_FAILED                = 0x00000006
+local CKR_ARGUMENTS_BAD                  = 0x00000007
+local CKR_NEED_TO_CREATE_THREADS         = 0x00000009
+local CKR_CANT_LOCK                      = 0x0000000A
+local CKR_DEVICE_ERROR                   = 0x00000030
+local CKR_DEVICE_MEMORY                  = 0x00000031
+local CKR_DEVICE_REMOVED                 = 0x00000032
+local CKR_SESSION_CLOSED                 = 0x000000B0
+local CKR_SESSION_COUNT                  = 0x000000B1
+local CKR_SESSION_HANDLE_INVALID         = 0x000000B3
+local CKR_SESSION_PARALLEL_NOT_SUPPORTED = 0x000000B4
+local CKR_SESSION_READ_WRITE_SO_EXISTS   = 0x000000B8
+local CKR_TOKEN_NOT_PRESENT              = 0x000000E0
+local CKR_TOKEN_NOT_RECOGNIZED           = 0x000000E1
+local CKR_TOKEN_WRITE_PROTECTED          = 0x000000E2
+local CKR_CRYPTOKI_NOT_INITIALIZED       = 0x00000190
+local CKR_CRYPTOKI_ALREADY_INITIALIZED   = 0x00000191
 
 local RV_ERR_MSG = {}
 RV_ERR_MSG[ CKR_HOST_MEMORY ] = "HSM driver memory allocation error"
+RV_ERR_MSG[ CKR_SLOT_ID_INVALID ] = "Unknown slot"
 RV_ERR_MSG[ CKR_GENERAL_ERROR ] = "HSM General error"
 RV_ERR_MSG[ CKR_FUNCTION_FAILED ] = "HSM processing failed" 
 RV_ERR_MSG[ CKR_ARGUMENTS_BAD ] = "Bad arguments provided"
 RV_ERR_MSG[ CKR_NEED_TO_CREATE_THREADS ] = "Need to create threads"
 RV_ERR_MSG[ CKR_CANT_LOCK ] = "Driver unable to create mutex" 
+RV_ERR_MSG[ CKR_DEVICE_ERROR ] = "HSM module error"
+RV_ERR_MSG[ CKR_DEVICE_MEMORY ] = "HSM module memory error"
+RV_ERR_MSG[ CKR_DEVICE_REMOVED ] = "HSM module removed"
+RV_ERR_MSG[ CKR_SESSION_CLOSED ] = "Session has been closed"
+RV_ERR_MSG[ CKR_SESSION_COUNT ] = "Too many sessions"
+RV_ERR_MSG[ CKR_SESSION_HANDLE_INVALID ] = "Invalid session handle"
+RV_ERR_MSG[ CKR_SESSION_PARALLEL_NOT_SUPPORTED ] = "CKF_SERIAL_SESSION flag must be set"
+RV_ERR_MSG[ CKR_SESSION_READ_WRITE_SO_EXISTS ] = "CKR_SESSION_READ_WRITE_SO_EXISTS"
+RV_ERR_MSG[ CKR_TOKEN_NOT_PRESENT ] = "Token not present"
+RV_ERR_MSG[ CKR_TOKEN_NOT_RECOGNIZED ] = "Token not recognized"
+RV_ERR_MSG[ CKR_TOKEN_WRITE_PROTECTED ] = "Token protected for writting"
+RV_ERR_MSG[ CKR_CRYPTOKI_NOT_INITIALIZED ] = "HSM module not initialized"
 RV_ERR_MSG[ CKR_CRYPTOKI_ALREADY_INITIALIZED ] = "HSM module already initialized"
 
 
-
 local function err_out ( ckr , err_prefix )
-  local err_string = RV_ERR_MSG[ckr] or ( "Unknown error: " .. ckr) 
-  return false, err_prefix .. err_string  
+  local err_string = RV_ERR_MSG[tonumber(ckr)] or ( "Unknown error: " .. tostring(ckr)) 
+  return nil, err_prefix .. err_string  
 end
 
 
@@ -68,9 +109,14 @@ local _M = {
 }
 local mt = { __index = _M }
 
+--Publicly available flags
 _M.CKF_SERIAL_SESSION = 0x00000004
+_M.CKF_RW_SESSION     = 0x00000002
 
-
+--User types 
+_M.CKU_SO             = 0
+_M.CKU_USER           = 1
+_M.CKU_CONTEXT_SPECIFIC = 2
 
 _M.init = function ( options ) 
 
@@ -109,6 +155,22 @@ _M.close = function ()
   return true
 end
 
+
+_M.close_all_sessions = function (slot_id)
+  if not _M.driver then
+    return nil, "Module not initialized"
+  end
+
+  local ckr = _M.driver.C_CloseAllSessions(slot_id)
+   
+  if not ckr or ckr ~= CKR_OK then
+    return err_out(ckr, "Failed to close sessions: ")
+  end
+
+  return true
+end
+
+
 _M.close_session = function(self)
   self.close = _M.close
   if not _M.driver then
@@ -131,7 +193,7 @@ _M.close_session = function(self)
   return true
 end
 
-_M.new_session = function (slotId, flags, application, notificationCallback)
+_M.new_session = function (slot_id, flags, application, notificationCallback)
   if not _M.driver then
     return nil, "Module not initialized"
   end
@@ -140,7 +202,7 @@ _M.new_session = function (slotId, flags, application, notificationCallback)
 
   local sessionHandlePtr = ffi.new('CK_SESSION_HANDLE[1]')
 
-  local ckr = _M.driver.C_OpenSession(slotId, flags, application, notificationCallback, sessionHandlePtr)
+  local ckr = _M.driver.C_OpenSession(slot_id, flags, application, notificationCallback, sessionHandlePtr)
   if not ckr or ckr ~= CKR_OK then
     return err_out(ckr, "Unable to open session: ")
   end
@@ -150,6 +212,56 @@ _M.new_session = function (slotId, flags, application, notificationCallback)
     _sessionHandle = sessionHandlePtr[0],
     close = _M.close_session
   }, mt)
+
+end
+
+
+_M.login = function ( self, pin , user_type )
+  
+  if not _M.driver then
+    return nil, "Module not initialized"
+  end
+
+  if not self or not self._sessionPtr then
+    return false, "Not valid session"
+  end
+
+  if not user_type then 
+    return false, "Invalid user_type"
+  end
+
+  if not pin or #pin < 4 then 
+    return false, "Invalid PIN"
+  end
+  
+  local cast_pin  = ffi.cast ('CK_UTF8CHAR_PTR', pin)
+
+  print ("cast_pin:", cast_pin[0])
+  print ("cast_pin:", cast_pin[1])
+  print ("cast_pin:", cast_pin[2])
+  print ("cast_pin:", cast_pin[3])
+  print ("pin len : ", #pin)
+
+  local ckr = _M.driver.C_Login(self._sessionHandle, user_type, cast_pin, #pin)
+  if not ckr or ckr ~= CKR_OK then
+    return err_out(ckr, "Unable to login: ")
+  end
+
+end
+
+_M.logout = function ( self ) 
+  if not _M.driver then
+    return nil, "Module not initialized"
+  end
+
+  if not self or not self._sessionPtr then
+    return false, "Not valid session"
+  end
+
+  local ckr = _M.driver.C_Logout(self._sessionHandle)
+  if not ckr or ckr ~= CKR_OK then
+    return err_out(ckr, "Logout processing error: ")
+  end
 
 end
 
--- src/resty/pkcs11_test.lua
+++ src/resty/pkcs11_test.lua
@@ -10,9 +10,26 @@ print ("Module initialized")
 
 local hsmSession, err = hsm_module.new_session(0x359af711)
 
-if not hsmSession then print ( err ) return end
+if not hsmSession then print ( err ) end
 
-print ( "Session opened = ", hsmSession._sessionHandle)
+print ( "Session opened = ", tonumber(hsmSession._sessionHandle))
+
+
+ok, err = hsmSession:login("1234", hsm_module.CKU_USER)
+if not ok then 
+  print (err)
+else
+  print ("Login succesfull")
+end
+
+ok, err = hsmSession:logout()
+if not ok then 
+  print (err)
+else
+  print ("Logout succesfull")
+end
+
+
 
 ok, err = hsmSession:close()
 if not ok then 
